0b095dd3295f2fafceaae59e10c2b1b29058eb63
TZGyn <ca538901d@mozmail.com>
2025-03-16 14:25:35 +0800

    update web reader ui

--- src/lib/components/message/tool/web-reader.svelte
+++ src/lib/components/message/tool/web-reader.svelte
@@ -29,38 +29,34 @@
 </script>
 
 {#if result.result}
-	<Accordion.Root type="single">
-		<Accordion.Item value="markdown">
-			<Accordion.Trigger
-				class={cn('p-4 shadow-sm hover:no-underline')}>
-				<div class="flex w-full items-center justify-between">
-					<div class="flex items-center gap-2">
-						<div
-							class="rounded-lg bg-neutral-100 p-2 dark:bg-neutral-800">
-							<LibraryBigIcon class="h-4 w-4 text-neutral-500" />
-						</div>
-						<div>
-							<h2 class="pr-4 text-start font-medium">
-								{result.result.data.title}
-							</h2>
-							<span
-								class="text-muted-foreground line-clamp-1 pr-4 text-start text-sm">
-								{result.result.data.url}
-							</span>
-						</div>
-					</div>
+	<div class="rounded border">
+		<div
+			class="bg-secondary flex w-full items-center justify-between p-2">
+			<div class="flex items-center gap-2">
+				<div
+					class="rounded-lg bg-neutral-100 p-2 dark:bg-neutral-800">
+					<LibraryBigIcon class="h-4 w-4 text-neutral-500" />
 				</div>
-			</Accordion.Trigger>
-			<Accordion.Content>
-				<div class={cn('bg-background w-fit rounded-xl')}>
-					<div
-						class="prose prose-neutral dark:prose-invert prose-p:my-0">
-						<Markdown
-							content={result.result.data.content}
-							id={`tool-${nanoid()}`} />
-					</div>
+				<div>
+					<h2 class="pr-4 text-start font-medium">
+						{result.result.data.title}
+					</h2>
+					<span
+						class="text-muted-foreground line-clamp-1 text-start text-sm">
+						{result.result.data.url}
+					</span>
 				</div>
-			</Accordion.Content>
-		</Accordion.Item>
-	</Accordion.Root>
+			</div>
+		</div>
+		<div
+			class={cn(
+				'bg-background max-h-[max(50vh,500px)] w-full overflow-scroll p-2',
+			)}>
+			<div class="prose prose-neutral dark:prose-invert prose-p:my-0">
+				<Markdown
+					content={result.result.data.content}
+					id={`tool-${nanoid()}`} />
+			</div>
+		</div>
+	</div>
 {/if}
