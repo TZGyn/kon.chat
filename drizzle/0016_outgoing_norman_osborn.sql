1b262e0647a2a90ca024b566155896efee2b7411
TZGyn <ca538901d@mozmail.com>
2025-07-07 15:46:41 +0800

    update 0016 migration to add created_at column

--- drizzle/0016_outgoing_norman_osborn.sql
+++ drizzle/0016_outgoing_norman_osborn.sql
@@ -1,5 +1,5 @@
 -- Custom SQL migration file, put your code below! --
 ALTER TABLE "user" RENAME COLUMN "created_at" TO "created_at_unix";
 ALTER TABLE "user" ALTER COLUMN "email" TYPE TEXT;
- 
+ALTER TABLE "user" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;
 
