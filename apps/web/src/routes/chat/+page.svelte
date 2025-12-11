1ed5719ccbeafd33b80c320cc1c23b7d4e9a4ddf
TZGyn <spectator_brethren761@slmails.com>
2025-12-11 12:03:34 +0800

    remove load function from chat route to allow prerender

---
+++ apps/web/src/routes/chat/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { goto } from "$app/navigation"
+	import { nanoid } from "$lib/nanoid"
+	import { onMount } from "svelte"
+
+	onMount(()=> {
+		goto(`/chat/${nanoid()}?type=new`)
+	})
+</script>
--- apps/web/src/routes/chat/+page.ts
+++
@@ -1,6 +0,0 @@
-import { nanoid } from '$lib/nanoid'
-import { redirect } from '@sveltejs/kit'
-
-export const load = async () => {
-	redirect(302, `/chat/${nanoid()}?type=new`)
-}
