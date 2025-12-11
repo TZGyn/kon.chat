334674f3e31f0f84963d65cb63983f6fbb29edf5
TZGyn <spectator_brethren761@slmails.com>
2025-12-11 11:08:10 +0800

    update backend dockerfile and drizzle config

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -2,10 +2,13 @@
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
-	schema: './src/routes/api/(src)/lib/db/schema.ts',
+	schema: './lib/db/schema.ts',
 	out: './drizzle',
 	dialect: 'postgresql',
 	dbCredentials: {
 		url: process.env.DATABASE_URL as string,
+	},
+	migrations: {
+		prefix: 'timestamp',
 	},
 })
